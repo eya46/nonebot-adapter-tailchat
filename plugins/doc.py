2eef1e0be0d91de23ac149547d0b768846154553
eya46 <61458340+eya46@users.noreply.github.com>
2024-05-07 22:45:02 +0800

    ruff

--- plugins/doc.py
+++ plugins/doc.py
@@ -9,8 +9,6 @@ doc = on_command("doc", rule=NotSelf)
 @doc.handle()
 async def handle_echo():
     await doc.send(
-        MessageSegment.url(
-            url="https://tailchat.msgbyte.com/zh-Hans/docs/intro", text="点击查看文档"
-        )
+        MessageSegment.url(url="https://tailchat.msgbyte.com/zh-Hans/docs/intro", text="点击查看文档")
         + MessageSegment.emoji("laughing")
     )
